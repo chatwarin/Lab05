918e8c3afbbe52ac9c379901073d17b7c4a8256a
chatwarin <chatwarin97@gmail.com>
2017-11-13 15:54:17 +0700

    11

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,16 +4,13 @@ class Program
 {
     static void Main()
     {
-        int x = 6;
-        int y = 4;
-
-        Console.WriteLine(y *= 2);
-        int z = y = 3;
-
-        Console.WriteLine(z);
-        Console.WriteLine(x |= 1);
-        Console.WriteLine(x += 3);
-        Console.WriteLine(x /= 2);
+        int x = 10, y = 5;
+        Console.WriteLine("x > y : " + (x > y));
+        Console.WriteLine("x < y : " + (x < y));
+        Console.WriteLine("x >= y : " + (x >= y));
+        Console.WriteLine("x <= y : " + (x <= y));
+        Console.WriteLine("x == y : " + (x == y));
+        Console.WriteLine("x != y : " + (x != y));
     }
 }
 
