cb723c635f560c18b3848c17b41fb5325fbbc11a
chatwarin <chatwarin97@gmail.com>
2017-11-13 15:08:34 +0700

    3

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,13 +4,14 @@ class Program
 {
     static void Main()
     {
-        sayHello();
+        sayHello("Hello World.");
         Console.ReadLine();
     }
 
-    static void sayHello()
+    static void sayHello(string str)
     {
-        Console.WriteLine("Hello World.");
+        Console.WriteLine(str);
     }
 }
+
 
