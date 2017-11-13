1e980656819e6d724e9698f16db8609bbd4964b9
chatwarin <chatwarin97@gmail.com>
2017-11-13 14:31:31 +0700

    1

---
+++ Lab05/Lab05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
+
+class Program
+{
+    static void Main()
+    {
+        // Create web client.
+        WebClient client = new WebClient();
+        // Download string.
+        string value = client.DownloadString("http://www.kmitl.ac.th");
+        // Write values.
+        Console.WriteLine("--- WebClient result ---");
+        Console.WriteLine(value.Length);
+        Console.WriteLine(value);
+        StreamWriter outtxt = new StreamWriter("webload.html");
+        outtxt.Write(value);
+        outtxt.Dispose();
+    }
+}
