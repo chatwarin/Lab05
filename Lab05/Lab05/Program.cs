419a972a9a5c60a9a5fffddbd5cdd17998058491
chatwarin <chatwarin97@gmail.com>
2017-11-13 18:46:57 +0700

    CountE

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-class Triangle
+class CountE
 {
-    static double TriangleArea(double totalW, double h)
-    {
-        double area =  h * 0.5 * totalW;
-        return area;
-    }
-
     static void Main()
     {
-        Console.Write("Enter width1: ");
-        double width1 = double.Parse(Console.ReadLine());
-        Console.Write("Enter width2: ");
-        double width2 = double.Parse(Console.ReadLine());
-        double totalW = width1 + width2; // ผลรวมของความยาวของด้านคู่ขนาน
-        Console.Write("Enter height: ");
-        double height = double.Parse(Console.ReadLine());
-        Console.WriteLine("The area of the triangle is {0:f2}.", TriangleArea(height,totalW));
+        Console.Write("Enter a string: ");
+        string s = Console.ReadLine();
+        int count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'E') count++;
+        }
+        Console.WriteLine("There are {0} E's in the string", count);
     }
 }
 
